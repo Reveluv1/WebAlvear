df2239f321ccb06481fd23129f66493560ce33d8
Reveluv1 <daniae@live.com>
2024-06-29 18:59:26 -0300

    Ultimo cambio 29/6 19hs

--- script.js
+++ script.js
@@ -14,3 +14,62 @@ function isMobileDevice() {
   window.onload = function() {
     redirectToWhatsApp();
   };
+
+
+// Selección del botón hamburguesa y del menú
+const toggleMenu = document.getElementById('menu-toggle');
+const menu = document.querySelector('.ul_links');
+
+// Agregar evento al botón hamburguesa para abrir y cerrar el menú
+toggleMenu.addEventListener('change', function() {
+  if (toggleMenu.checked) {
+    // Si el menú se abre, añade la clase 'menu-abierto' al cuerpo
+    document.body.classList.add('menu-abierto');
+  } else {
+    // Si el menú se cierra, remueve la clase 'menu-abierto' del cuerpo
+    document.body.classList.remove('menu-abierto');
+  }
+});
+
+
+
+// Seleccionar todos los enlaces del menú de navegación
+const links = document.querySelectorAll('.ul_links a');
+
+// Agregar evento de clic a cada enlace
+links.forEach(enlace => {
+  enlace.addEventListener('click', function(e) {
+    e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+
+    const href = this.getAttribute('href'); // Obtener el valor del atributo href del enlace
+    const target = document.querySelector(href); // Seleccionar el elemento destino basado en el href
+
+    if (target) {
+      // Calcular la posición de desplazamiento
+      const offsetTop = target.offsetTop - 75;
+      toggleMenu.checked = false;
+      document.body.classList.remove('menu-abierto');
+      // Realizar el desplazamiento suave usando window.scrollTo
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth' // Opciones de desplazamiento suave
+      });
+    }
+  });
+});
+
+
+
+const logo = document.querySelector('.logo-link');
+
+// Agregar evento de clic al logo
+logo.addEventListener('click', function(e) {
+    document.body.classList.remove('menu-abierto');
+    e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+    toggleMenu.checked = false;
+     // Realizar el desplazamiento suave hacia la parte superior de la página
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Opciones de desplazamiento suave
+  });
+});
